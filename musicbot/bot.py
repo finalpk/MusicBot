75a28d8b058370b0afd8f65f68ff47b4a5e2e2ca
Marius <finalpkpb@gmail.com>
2017-04-16 18:44:40 +0200

    .

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -1472,10 +1472,10 @@ class MusicBot(discord.Client):
         if player.is_stopped:
             raise exceptions.CommandError("Can't skip! The player is not playing!", expire_in=20)
 
-        if (author.id == self.config.owner_id
-                or permissions.instaskip
-                or author == player.current_entry.meta.get('author', None)):
-            raise exceptions.CommandError("You are not allowed to use this command\n" + "\nAuthor ID : " + author.id + "\nOwner: " + self.config.owner_id, expire_in=20)
+        if (author.id != self.config.owner_id
+                or not permissions.instaskip
+                or author != player.current_entry.meta.get('author', None)):
+            raise exceptions.CommandError("You are not allowed to use this command", expire_in=10)
 
         if not player.current_entry:
             if player.playlist.peek():
